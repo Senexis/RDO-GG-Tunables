4c948e10f7e8017ed383d2195911afcf370dc2f2
Senexis <timoviveen@gmail.com>
2023-02-19 22:10:19 +0100

    Try this out.

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    copyPublicDir: false,
-    emptyOutDir: false,
-    outDir: fileURLToPath(new URL("./build", import.meta.url)),
-    rollupOptions: {
-      input: {
-        index: fileURLToPath(new URL("./index.html", import.meta.url)),
-      },
-    },
-  },
   plugins: [vue()],
   define: {
     APP_VERSION: JSON.stringify(version),
@@ -25,8 +15,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  server: {
-    open: "/index.html",
-  },
-  base: "/",
 });
