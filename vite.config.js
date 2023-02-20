3f83c41cf4ec090f0bc5d58b4b710eb48dc41197
Senexis <timoviveen@gmail.com>
2023-02-20 13:42:33 +0100

    Trim app version.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const commit = execSync("git rev-parse --short HEAD").toString();
+const commit = execSync("git rev-parse --short HEAD").toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
