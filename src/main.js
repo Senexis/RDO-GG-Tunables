ad97a05a7a228b8fde81668ee963220750a5d6c7
Senexis <timoviveen@gmail.com>
2023-02-25 15:21:31 +0100

    Add Sentry for error collection.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 import "jsondiffpatch/dist/formatters-styles/html.css";
 import "./assets/main.css";
@@ -10,6 +13,17 @@ const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: SENTRY_DSN,
+  integrations: [
+    new BrowserTracing({
+      tracePropagationTargets: ["localhost", "tunables.rdo.gg", /^\//],
+    }),
+  ],
+  tracesSampleRate: 0.3,
+});
 
 app.use(pinia);
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,31 @@
+/* eslint-disable no-undef */
 import { execSync } from "node:child_process";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 const commit = execSync("git rev-parse --short HEAD").toString().trim();
 const change = execSync("git log -1 --pretty=%B").toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORGANIZATION,
+      project: process.env.SENTRY_PROJECT,
+      include: "./dist",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   define: {
     APP_VERSION: JSON.stringify(commit),
     APP_CHANGE: JSON.stringify(change),
+    SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
   },
   resolve: {
     alias: {
