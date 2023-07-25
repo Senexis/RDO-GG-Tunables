b93e84bd6b9e92977ce289852ac7d9d35e81f7ec
Senexis <timoviveen@gmail.com>
2023-07-25 15:34:38 +0200

    Stop using outdated AJV-CLI.

---
+++ .github/scripts/validate_json.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const Ajv = require("ajv");
+
+const ajv = new Ajv();
+const dataPath = path.join(__dirname, "..", "..", "public", "data");
+const schemaPath = path.join(__dirname, "..", "..", "schemas");
+
+const filesToValidate = {
+    'hash_dictionary': [
+        'labels',
+    ],
+    'string_array': [
+        'daily_objectives',
+        'hsw_time_trials',
+        'rc_time_trials',
+        'time_trials',
+    ],
+    'tunable_types': [
+        'tunable_types',
+    ],
+};
+
+let isValid = true;
+for (const schemaName in filesToValidate) {
+    const schema = require(path.join(schemaPath, `${schemaName}.json`));
+    const validate = ajv.compile(schema);
+
+    for (const fileName of filesToValidate[schemaName]) {
+        const data = require(path.join(dataPath, `${fileName}.json`));
+        const valid = validate(data);
+
+        if (valid) {
+            console.log(`✅ ${schemaName}: ${fileName}.json`);
+            continue;
+        }
+
+        console.error(`❌ ${schemaName}: ${fileName}.json\n\n${JSON.stringify(validate.errors, null, "  ")}\n`);
+        isValid = false;
+    }
+}
+
+if (!isValid) {
+    console.error("\nValidation failed.");
+    process.exit(1);
+} else {
+    console.log("\nValidation succeeded.");
+    process.exit(0);
+}
