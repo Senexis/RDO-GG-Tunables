88b946053093cc5584f29efe4c65d9189b53c4a6
Senexis <timoviveen@gmail.com>
2023-12-23 14:40:24 +0100

    There is nothing in this commit. 🎄

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import FloatingVue from 'floating-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import hotkeys from 'hotkeys-js';
+import confetti from 'canvas-confetti';
 
 FloatingVue.options.distance = 10;
 import 'floating-vue/dist/style.css';
@@ -74,3 +75,61 @@ const fpPromise = FingerprintJS.load({ monitoring: false });
   app.use(FloatingVue);
   app.mount('#app');
 })();
+
+// One of the downsides of being open source? You can't hide your surprises.
+// Of course, there is no surprise here. So I don't know what this is about.
+// If you do end up seeing this, happy holidays!
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+try {
+  const today = new Date();
+  const christmas = new Date(today.getFullYear(), 11, 24);
+  const boxing = new Date(today.getFullYear(), 11, 26);
+
+  if (today >= christmas && today <= boxing) {
+    var duration = Number.POSITIVE_INFINITY;
+    var animationEnd = Date.now() + duration;
+    var skew = 1;
+
+    (function frame() {
+      var timeLeft = animationEnd - Date.now();
+      var ticks = Math.max(200, 500 * (timeLeft / duration));
+      skew = Math.max(0.8, skew - 0.001);
+
+      confetti({
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: ticks,
+        origin: {
+          x: Math.random(),
+          y: Math.random() * skew - 0.2,
+        },
+        colors: ['#ffffff'],
+        shapes: ['circle'],
+        gravity: randomInRange(0.4, 0.6),
+        scalar: randomInRange(0.4, 1),
+        drift: randomInRange(-0.4, 0.4),
+
+        // Appear behind the content
+        zIndex: -1,
+
+        // Improve performance
+        useWorker: true,
+
+        // Allow the user to opt-out of the animation
+        disableForReducedMotion: true,
+      });
+
+      if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+      }
+    })();
+  }
+} catch (error) {
+  // This is just a fun little surprise. If it fails, it fails.
+  // Of course, you'd be very sad to miss out on this surprise.
+  console.log('Something happened while trying to surprise you with some snowfall. Happy holidays regardless! 🎄');
+  console.error(error);
+}
